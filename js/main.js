64b684a52d163be22870efbfe252a8982862f0dd
Bianca W <bianca-git@users.noreply.github.com>
2022-01-09 09:34:13 +1100

    updated

--- js/main.js
+++ js/main.js
@@ -1,27 +1,28 @@
 const hsluv = require("hsluv")
 
-export const rainbowBackground = () => {
-  const viewportHeight = window.innerHeight
-  const contentHeight = document.body.getBoundingClientRect().height
-  const viewportsPerRotation = Math.min(
-    3,
-    contentHeight / viewportHeight
-  )
-  const from = 280
-  const progress =
-    window.scrollY / (viewportHeight * viewportsPerRotation)
-  const h = (from + 360 * progress) % 360
 
-  document.body.style.backgroundColor = hsluv.hsluvToHex([h, 100, 5])
-  document.getElementById('menu').style.backgroundColor = hsluv.hsluvToHex([h, 100, 5])
-  const sectionsCount = document.getElementsByTagName('section');
-  var loop;
-  for (loop = 0; loop < sectionsCount.length; loop++) {
-    sectionsCount[loop].style.backgroundColor = hsluv.hsluvToHex([h, 100, 15])
-    sectionsCount[loop].style.borderColor = hsluv.hsluvToHex([h, 100, 5])
-  }
+  function rainbowBackground() {
+    const viewportHeight = window.innerHeight
+    const contentHeight = document.body.getBoundingClientRect().height
+    const viewportsPerRotation = Math.min(
+      3,
+      contentHeight / viewportHeight
+    )
+    const from = 280
+    const progress =
+      window.scrollY / (viewportHeight * viewportsPerRotation)
+    const h = (from + 360 * progress) % 360
   
-}
-window.addEventListener('scroll', rainbowBackground, { passive: true })
-return () => window.removeEventListener('scroll', rainbowBackground)
+    document.body.style.backgroundColor = hsluv.hsluvToHex([h, 100, 5])
+    document.getElementById('menu').style.backgroundColor = hsluv.hsluvToHex([h, 100, 5])
+    const sectionsCount = document.getElementsByTagName('section');
+    var loop;
+    for (loop = 0; loop < sectionsCount.length; loop++) {
+      sectionsCount[loop].style.backgroundColor = hsluv.hsluvToHex([h, 100, 15])
+      sectionsCount[loop].style.borderColor = hsluv.hsluvToHex([h, 100, 5])
+    }
 
+    
+  }
+  window.addEventListener('scroll', rainbowBackground, { passive: true })
+  return () => window.removeEventListener('scroll', rainbowBackground)
