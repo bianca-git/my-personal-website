6a571dbe0f5c6c244e7c886991a50d892de5ed85
Bianca W <bianca-git@users.noreply.github.com>
2022-01-09 09:38:58 +1100

    Fixed JS

--- js/main.js
+++ js/main.js
@@ -1,28 +1,25 @@
 const hsluv = require("hsluv")
 
+function rainbowBackground() {
+  const viewportHeight = window.innerHeight
+  const contentHeight = document.body.getBoundingClientRect().height
+  const viewportsPerRotation = Math.min(
+    3,
+    contentHeight / viewportHeight
+  )
+  const from = 280
+  const progress =
+    window.scrollY / (viewportHeight * viewportsPerRotation)
+  const h = (from + 360 * progress) % 360
 
-  function rainbowBackground() {
-    const viewportHeight = window.innerHeight
-    const contentHeight = document.body.getBoundingClientRect().height
-    const viewportsPerRotation = Math.min(
-      3,
-      contentHeight / viewportHeight
-    )
-    const from = 280
-    const progress =
-      window.scrollY / (viewportHeight * viewportsPerRotation)
-    const h = (from + 360 * progress) % 360
-  
-    document.body.style.backgroundColor = hsluv.hsluvToHex([h, 100, 5])
-    document.getElementById('menu').style.backgroundColor = hsluv.hsluvToHex([h, 100, 5])
-    const sectionsCount = document.getElementsByTagName('section');
-    var loop;
-    for (loop = 0; loop < sectionsCount.length; loop++) {
-      sectionsCount[loop].style.backgroundColor = hsluv.hsluvToHex([h, 100, 15])
-      sectionsCount[loop].style.borderColor = hsluv.hsluvToHex([h, 100, 5])
-    }
-
-    
+  document.body.style.backgroundColor = hsluv.hsluvToHex([h, 100, 5])
+  document.getElementById('menu').style.backgroundColor = hsluv.hsluvToHex([h, 100, 5])
+  const sectionsCount = document.getElementsByTagName('section');
+  var loop;
+  for (loop = 0; loop < sectionsCount.length; loop++) {
+    sectionsCount[loop].style.backgroundColor = hsluv.hsluvToHex([h, 100, 15])
+    sectionsCount[loop].style.borderColor = hsluv.hsluvToHex([h, 100, 5])
   }
-  window.addEventListener('scroll', rainbowBackground, { passive: true })
   return () => window.removeEventListener('scroll', rainbowBackground)
+}
+window.addEventListener('scroll', rainbowBackground, { passive: true })
